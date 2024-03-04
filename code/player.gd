a664ed9bc29321442d72d9bf7c2a99a44583c385
Andallfor <leozwang2005@gmail.com>
2024-03-03 22:20:57 -0500

    animations
    palette cleanser

--- code/player.gd
+++ code/player.gd
@@ -4,16 +4,32 @@ extends Node2D
 @export var speedHorz: int = 50;
 
 var height: float = 10;
-
 var isInTransition: bool = false;
+var _isFirstRun: bool = true;
+
+var sprite: AnimatedSprite2D;
+
+var animationPriority: Array[String] = ["shoot", "reload", "run", "idle"];
+var animationSpeed = {
+	"shoot": 2,
+	"reload": 1,
+	"run": 1.5,
+	"idle": 1
+};
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.position = %spawn.position;
+	
+	sprite = get_node("sprite");
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var vec: Vector2 = Vector2();
+	if (_isFirstRun): # one frame delay to allow nav server to sync
+		_isFirstRun = false;
+		return;
+
+	var vec: Vector2 = Vector2(0, 0);
 
 	if (Input.is_action_pressed("move_up")):
 		vec.y -= speedVert;
@@ -23,14 +39,26 @@ func _physics_process(delta):
 	
 	if (Input.is_action_pressed("move_right")):
 		vec.x += speedVert;
+		sprite.flip_h = false;
 	
 	if (Input.is_action_pressed("move_left")):
 		vec.x -= speedVert;
+		sprite.flip_h = true;
+	
+	if (Input.is_action_just_pressed("shoot")):
+		tryPlayAnimation("shoot");
+	
+	if (sprite.animation == "shoot" && sprite.frame_progress != 1):
+		vec = Vector2(0, 0);
+	
+	if (vec.length() != 0):
+		tryPlayAnimation("run");
+	else:
+		tryPlayAnimation("idle", ["run"]);
 	
 	vec *= delta;
 	
 	# check if we're in a ramp transition for above and below levels
-	var space = get_world_2d().direct_space_state;
 	# note that height at mod 10 is considered to be at the surface of that tile layer
 	# height 0 is the surface of the first tile layer, not the bottom
 	var level: int = int(height / 10);
@@ -73,3 +101,13 @@ func checkTransition(mask: int) -> bool:
 	var result: Array[Dictionary] = get_world_2d().direct_space_state.intersect_point(point, 1);
 	
 	return result.size() != 0;
+
+func tryPlayAnimation(name: String, ignore: Array[String] = []):
+	# shush
+	# if new priority is greater than old priority (0 is greatest priority)
+	var isDone: bool = sprite.frame_progress == 1;
+	var override: bool = ignore.has(sprite.animation);
+	var priority: bool = animationPriority.find(name) < animationPriority.find(sprite.animation);
+	if (isDone || override || priority):
+		sprite.play(name);
+		sprite.speed_scale = animationSpeed[name];
--- code/testNavRegion.gd
+++ code/testNavRegion.gd
@@ -3,4 +3,5 @@ extends NavigationRegion2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(self.name + " " + str(self.get_region_rid()) + " " + str(self.get_navigation_map()) + " " + str(self.navigation_polygon));
+	#print(self.name + " " + str(self.get_region_rid()) + " " + str(self.get_navigation_map()) + " " + str(self.navigation_polygon));
+	pass
